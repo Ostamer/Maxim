623e556d93b179e73370bb269e2bae83e73d962f
Ostamer <arturik_sagadeev@mail.ru>
2025-11-06 15:27:52 +0500

    Добавлены сообщения для assert

--- tests/test_artwork_by_id.py
+++ tests/test_artwork_by_id.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic import ValidationError
 
 from config import VALID_OBJECT_ID, INVALID_OBJECT_ID
+from constants import ASSERT_MESSAGES
 from models import Artwork
 from utils import get_artwork_by_id
 
@@ -9,19 +10,37 @@ from utils import get_artwork_by_id
 # Тест получения существующего произведения искусства по ID
 def test_get_artwork_by_correct_id():
     response = get_artwork_by_id(VALID_OBJECT_ID)
-    assert response.status_code == 200
+    assert response.status_code == 200, ASSERT_MESSAGES["status_200"].format(
+        status=response.status_code
+    )
     data = response.json()
     try:
         artwork = Artwork(**data)
-        assert artwork.objectID == VALID_OBJECT_ID
-        assert artwork.title != ""
-        assert isinstance(artwork.isPublicDomain, bool)
-        assert isinstance(artwork.isHighlight, bool)
+        assert artwork.objectID == VALID_OBJECT_ID, (
+            ASSERT_MESSAGES["id_doesnt_match"].format(
+                actual=artwork.objectID, expected=VALID_OBJECT_ID
+            )
+        )
+        assert artwork.title != "", ASSERT_MESSAGES["empty_field_title"]
+        assert isinstance(artwork.isPublicDomain, bool), (
+            ASSERT_MESSAGES["invalid_public_domain_field_type"].format(
+                type=type(artwork.isPublicDomain)
+            )
+        )
+        assert isinstance(artwork.isHighlight, bool), (
+            ASSERT_MESSAGES["invalid_highlight_field_type"].format(
+                type=type(artwork.isHighlight)
+            )
+        )
     except ValidationError as e:
-        pytest.fail(f"Ошибка: {e}")
+        pytest.fail(ASSERT_MESSAGES['validation_model_error'].format(
+            error=e
+        ))
 
 
 # Тест получения несуществующего произведения искусства по ID
 def test_get_artwork_by_invalid_id():
     response = get_artwork_by_id(INVALID_OBJECT_ID)
-    assert response.status_code == 404
+    assert response.status_code == 404, ASSERT_MESSAGES['status_404'].format(
+        status=response.status_code
+    )
